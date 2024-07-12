bf88578dc124046fffa62c8993ceeb3b4a5ba8c1
Alexander Suvorov <51918244+alexsuvo@users.noreply.github.com>
2024-07-12 16:39:40 +0100

    Update qpilot.py

--- qpilot.py
+++ qpilot.py
@@ -18,17 +18,17 @@ class QPilot:
         self.model = mlflow.pyfunc.load_model(os.environ.get('CUST_MDL_URL'))
         self.logger.debug('Model loaded')
 
-        self.tokenizer = AutoTokenizer.from_pretrained(os.environ.get('BASE_MDL_NAME'), token=os.environ.get('HF_TOKEN'), model_max_length=os.environ.get('CUTOFF_LEN'), padding_side="right")
+        self.tokenizer = AutoTokenizer.from_pretrained(os.environ.get('BASE_MDL_NAME'), token=os.environ.get('HF_TOKEN'), model_max_length=int(os.environ.get('CUTOFF_LEN')), padding_side="right")
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.logger.debug('Tokenizer loaded')
 
     def get_output(self, text):
-        inputs = self.tokenizer.encode_plus(text, return_tensors="pt", padding="longest", truncation=True, max_length=os.environ.get('CUTOFF_LEN'))
+        inputs = self.tokenizer.encode_plus(text, return_tensors="pt", padding="longest", truncation=True, max_length=int(os.environ.get('CUTOFF_LEN')))
 
         input_ids = inputs["input_ids"].cuda()
         attention_mask = inputs["attention_mask"].cuda()
 
-        output = self.model.generate(input_ids, attention_mask=attention_mask, max_length=os.environ.get('CUTOFF_LEN'), generation_config=GenerationConfig(temperature=0.001, top_p=1.0, num_beams=1, pad_token_id=self.tokenizer.eos_token_id))
+        output = self.model.generate(input_ids, attention_mask=attention_mask, max_length=int(os.environ.get('CUTOFF_LEN')), generation_config=GenerationConfig(temperature=0.001, top_p=1.0, num_beams=1, pad_token_id=self.tokenizer.eos_token_id))
 
         return self.tokenizer.decode(output[0], skip_special_tokens=True)
 
