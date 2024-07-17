84ff86736224ffbd8f3db56aa1b629e4c87251e6
Alexander Suvorov <51918244+alexsuvo@users.noreply.github.com>
2024-07-17 13:14:36 +0100

    Update qpilot.py

--- qpilot.py
+++ qpilot.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 import mlflow
@@ -7,24 +6,20 @@ from ray import serve
 from starlette.requests import Request
 from transformers import AutoTokenizer, GenerationConfig
 
-logging.basicConfig(level=logging.INFO)
-
 
 @serve.deployment
 class QPilot:
 
     def __init__(self):
-        self.logger = logging.getLogger(__name__)
-
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.logger.info(f'### Device:{self.device}')
+        print(f'### Device:{self.device}')
 
         self.model = mlflow.transformers.load_model(os.environ.get('CUST_MDL_URL'))
-        self.logger.info('### Model loaded')
+        print('### Model loaded')
 
         self.tokenizer = AutoTokenizer.from_pretrained(os.environ.get('BASE_MDL_NAME'), token=os.environ.get('HF_TOKEN'), model_max_length=int(os.environ.get('CUTOFF_LEN')), padding_side="right")
         self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.logger.info('### Tokenizer loaded')
+        print('### Tokenizer loaded')
 
     def get_output(self, text):
         torch.cuda.empty_cache()
@@ -34,16 +29,17 @@ class QPilot:
         input_ids = inputs["input_ids"].to(self.device)
         attention_mask = inputs["attention_mask"].to(self.device)
 
-        output = self.model.model.generate(input_ids, attention_mask=attention_mask, max_length=int(os.environ.get('CUTOFF_LEN')), generation_config=GenerationConfig(temperature=0.001, top_p=1.0, num_beams=1, pad_token_id=self.tokenizer.eos_token_id))
+        output = self.model.model.generate(input_ids, attention_mask=attention_mask, max_length=int(os.environ.get('CUTOFF_LEN')),
+                                           generation_config=GenerationConfig(temperature=0.001, top_p=1.0, num_beams=1, pad_token_id=self.tokenizer.eos_token_id))
 
         return self.tokenizer.decode(output[0], skip_special_tokens=True)
 
     async def __call__(self, http_request: Request) -> str:
         req = await http_request.json()
-        self.logger.info(f'### Request body: {req}')
+        print(f'### Request body: {req}')
 
         question = req['question']
-        self.logger.info(f'### Question:{question}')
+        print(f'### Question:{question}')
 
         prompt = f"""<|begin_of_text|>
         <|start_header_id|>system<|end_header_id|>You are a powerful Q language question-answering system.<|eot_id|>
@@ -51,7 +47,7 @@ class QPilot:
         <|start_header_id|>system<|end_header_id|>Do not add Notes or examples.<|eot_id|>
         <|start_header_id|>user<|end_header_id|>Q language question:{question}<|eot_id|>"""
         answer = self.get_output(prompt)
-        self.logger.info(f'### Answer:{answer}')
+        print(f'### Answer:{answer}')
 
         return answer
 
